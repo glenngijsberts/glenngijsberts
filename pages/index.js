be4302f483c07fa5f2403df4ddbb88578e58adc6
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-11-09 23:36:56 +0100

    Next seo setup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react'
+import { NextSeo } from 'next-seo'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Glenn Gijsberts - Portfolio</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  </div>
-);
+export default () => (
+  <>
+    <NextSeo
+      title="Glenn Gijsberts - Portfolio 👨‍💻"
+      description="Glenn Gijsberts - Interaction Design student en Front-end developer"
+      openGraph={{
+        url: 'https://glenngijsberts.now.sh/',
+        title: 'Glenn Gijsberts Portfolio',
+        description: 'Glenn Gijsberts - Interaction Design student en Front-end developer',
+        images: [{ url: 'https://glenngijsberts.now.sh/img/glenngijsberts.jpg' }],
+        site_name: 'Glenn Gijsberts Portfolio',
+      }}
+      twitter={{
+        handle: '@glenngijsberts',
+        cardType: 'summary_large_image',
+        image: 'https://glenngijsberts.now.sh/img/glenngijsberts.jpg',
+      }}
+    />
 
-export default Home;
+    <h1>Hello world!!!</h1>
+  </>
+)
